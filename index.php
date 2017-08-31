3b0985f18f5e9e6ccf233b21f7d3f04e18d3a1ff
Paul Willis <info@paulwillis.com>
2017-08-31 13:25:36 +0100

    Version up and CSS padding

--- index.php
+++ index.php
@@ -10,11 +10,14 @@
 				font-family: "Helvetica Neue", Arial, Helvetica, Geneva, sans-serif;
 				color: white;
 			}
+			h1 {
+				margin-top: 100px;
+			}
 		</style>
 </head>
 <body>
 <h1>CRC Auto deploy demo</h1>
-<h2><?php echo "This is PHP revision 8"; ?></h2>
+<h2><?php echo "This is PHP revision 9"; ?></h2>
 <h2><?php echo date('l jS \of F Y h:i:s A'); ?></h2>
 </body>
 </html>
